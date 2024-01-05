55968aa3184f1f14d037f72c7a2efc18a6933486
Harrison Stephan <harrisonstephan@Harrisons-iMac.local>
2024-01-05 14:07:04 -0700

    added the base of the html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=43.5788&lon=116.5598&appid=16a0d06fe2bb273f50b9f98ac2bdb5a3";
+const weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=43.5788&lon=-116.5598&appid=16a0d06fe2bb273f50b9f98ac2bdb5a3";
 
 function findWeather() {
     fetch(weatherUrl)
