3f909ff88e9ff2ce73fad7f1e0d5e2000a09357c
Harrison Stephan <harrisonstephan@Harrisons-iMac.local>
2024-01-05 13:50:06 -0700

    added the weather API

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=43.5788&lon=116.5598&appid=16a0d06fe2bb273f50b9f98ac2bdb5a3";
+
+function findWeather() {
+    fetch(weatherUrl)
+        .then (function (response) {
+            return response.json();
+        })
+        .then (function (data) {
+            console.log(data);
+        })
+};
+
+findWeather();
